e0573c2984d3e05a2bc4465946f6cfdc6d9c99e9
dheeraj-kushakula <82694526+dheeraj-kushakula@users.noreply.github.com>
2024-09-25 15:46:30 -0400

    Derive table from existing table

---
+++ Beginner-data-analysis/Derive points table from icc analysis.sql
@@ -0,0 +1,48 @@
+create table icc_world_cup
+(
+Team_1 Varchar(20),
+Team_2 Varchar(20),
+Winner Varchar(20)
+);
+INSERT INTO icc_world_cup values('India','SL','India');
+INSERT INTO icc_world_cup values('SL','Aus','Aus');
+INSERT INTO icc_world_cup values('SA','Eng','Eng');
+INSERT INTO icc_world_cup values('Eng','NZ','NZ');
+INSERT INTO icc_world_cup values('Aus','India','India');
+
+select * from icc_world_cup;
+
+select Team_1 as Team_Name, case when Team_1=Winner then 1 else 0 end as win_flag
+from icc_world_cup
+union all 
+select Team_2 as Team_Name, case when TEam_2=Winner then 1 else 0 end as win_flag
+from icc_world_cup;
+
+
+select Team_Name,count(1) as Matches_played, sum(win_flag) as no_of_matches_won, count(1) - sum(win_flag) as no_of_losses from
+(select Team_1 as Team_Name, case when Team_1=Winner then 1 else 0 end as win_flag
+from icc_world_cup
+union all 
+select Team_2 as Team_Name, case when TEam_2=Winner then 1 else 0 end as win_flag
+from icc_world_cup) as a
+group by Team_Name 
+order by no_of_matches_won desc;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
